c846e7497670197fe44a7ab7065113cf46863641
oleg425top <oleg59152@gmail.com>
2026-01-28 15:17:35 +0500

    Initial commit

---
+++ main/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Категория')
+    slug = models.CharField(max_length=50, unique=True, verbose_name='Слаг')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str(self):
+        return self.name
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Размер')
+
+    def __str__(self):
+        return self.name
+
+
+class ProductSize(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product_size', verbose_name='Товар')
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, verbose_name='Размер')
+    stock = models.PositiveIntegerField(default=0, verbose_name='Доступное кол-во')
+
+    def __str__(self):
+        return f'{self.size.name} ({self.stock} в наличии) для: {self.product.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Категория')
+    slug = models.CharField(max_length=50, unique=True, verbose_name='Слаг')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
+    color = models.CharField(max_length=100, verbose_name='Цвет')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(max_length=200, blank=True, null=True)
+    image = models.ImageField(upload_to='products/main/', verbose_name='Изображение')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images', verbose_name='Товар')
+    image = models.ImageField(upload_to='product/extra', verbose_name='Изображение')
+
