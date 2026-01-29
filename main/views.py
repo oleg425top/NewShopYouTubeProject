c9630374556f67a1f19c32cf468356b2cdeb1b2c
oleg425top <oleg59152@gmail.com>
2026-01-29 16:05:11 +0500

    1. Удалил лишние импорты

--- main/views.py
+++ main/views.py
@@ -1,6 +1,3 @@
-from itertools import product
-from timeit import template
-
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView, DetailView
 from django.http import HttpResponse
