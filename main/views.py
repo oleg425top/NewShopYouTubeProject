5a73de9f2bff50147a42aae83dde38896b03c2fc
oleg425top <oleg59152@gmail.com>
2026-01-29 15:11:55 +0500

    1. Добавил вьюшку для главной страницы сайта

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView, DetailView
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+from .models import Category, Product, Size
+from django.db.models import Q
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'main/base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['current_category'] = None
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.headers.get('HX-Request'):
+            return TemplateResponse(request, 'main/home.html', context)
+        return TemplateResponse(request, self.template_name, context)
